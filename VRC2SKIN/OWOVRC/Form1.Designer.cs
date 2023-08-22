08221139be502c1b1f4d1991ab04cb7b2177b612
holadivinus <holadivinus@gmail.com>
2023-08-22 01:18:46 -0700

    initial commit

---
+++ VRC2SKIN/OWOVRC/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace OWOVRC
+{
+    partial class OWO_VRC_FORM
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(OWO_VRC_FORM));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cmdArgLabel = new System.Windows.Forms.Label();
+            this.debug = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(305, 104);
+            this.label1.TabIndex = 0;
+            this.label1.Text = resources.GetString("label1.Text");
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 122);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(100, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "CommandLine args:";
+            // 
+            // cmdArgLabel
+            // 
+            this.cmdArgLabel.AutoSize = true;
+            this.cmdArgLabel.Location = new System.Drawing.Point(12, 135);
+            this.cmdArgLabel.Name = "cmdArgLabel";
+            this.cmdArgLabel.Size = new System.Drawing.Size(51, 13);
+            this.cmdArgLabel.TabIndex = 2;
+            this.cmdArgLabel.Text = "args here";
+            // 
+            // debug
+            // 
+            this.debug.AutoSize = true;
+            this.debug.Location = new System.Drawing.Point(12, 173);
+            this.debug.Name = "debug";
+            this.debug.Size = new System.Drawing.Size(109, 13);
+            this.debug.TabIndex = 3;
+            this.debug.Text = "debug msg goes here";
+            // 
+            // OWO_VRC_FORM
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(338, 258);
+            this.Controls.Add(this.debug);
+            this.Controls.Add(this.cmdArgLabel);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "OWO_VRC_FORM";
+            this.Text = "OWO VRC";
+            this.Load += new System.EventHandler(this.OWO_VRC_FORM_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label cmdArgLabel;
+        private System.Windows.Forms.Label debug;
+    }
+}
+
---
+++ VRC2SKIN/OWOVRC/Form1.cs
@@ -0,0 +1,140 @@
+﻿using SharpOSC;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using OWOGame;
+
+namespace OWOVRC
+{
+    public partial class OWO_VRC_FORM : Form
+    {
+        public OWO_VRC_FORM()
+        {
+            InitializeComponent();
+        }
+
+        private static string VRCIP;
+        private static int VRCPORT;
+        private static string OWOIP;
+
+        private class OWOTriggerer
+        {
+            public OWOTriggerer(Muscle muscle) => Muscle = muscle;
+            public volatile bool curState;
+            public Muscle Muscle;
+        }
+        private static BakedSensation bs = BakedSensation.Parse($"0~Touch~80,3,60,0,0,0,~{OWOGame.Icon.Environment}");
+        private static readonly Dictionary<string, OWOTriggerer> Triggers = new Dictionary<string, OWOTriggerer>() 
+        {
+            {"Pectoral_R", new OWOTriggerer(Muscle.Pectoral_R)},
+            {"Pectoral_L", new OWOTriggerer(Muscle.Pectoral_L)},
+            {"Abdominal_R", new OWOTriggerer(Muscle.Abdominal_R)},
+            {"Arm_R", new OWOTriggerer(Muscle.Arm_R)},
+            {"Arm_L", new OWOTriggerer(Muscle.Arm_L)},
+            {"Dorsal_R", new OWOTriggerer(Muscle.Dorsal_R)},
+            {"Dorsal_L", new OWOTriggerer(Muscle.Dorsal_L)},
+            {"Lumbar_R", new OWOTriggerer(Muscle.Lumbar_R)},
+            {"Lumbar_L", new OWOTriggerer(Muscle.Lumbar_L)}
+        };
+        private void OWO_VRC_FORM_Load(object sender, EventArgs e)
+        {
+#if DEBUG
+            string[] cmdArgs = Environment.GetCommandLineArgs().Skip(1).ToArray();
+            cmdArgLabel.Text = string.Join(" ", cmdArgs);
+            for (int i = 0; i < cmdArgs.Length; i++)
+            {
+                if (i + 1 == cmdArgs.Length)
+                    break;
+
+                string curArg = cmdArgs[i];
+                string curVal = cmdArgs[i + 1];
+                switch (curArg)
+                {
+                    case "-OWOIP":
+                        OWOIP = curVal;
+                        break;
+                    case "-VRCIP":
+                        VRCIP = curVal;
+                        break;
+                    case "-VRCPORT":
+                        VRCPORT = int.Parse(curVal);
+                        break;
+                }
+            }
+#else
+            OWOIP = "127.0.0.1";
+            VRCIP = "127.0.0.1";
+            VRCPORT = 9001;
+            cmdArgLabel.Text = "this is the version that uses default stuff" + Environment.NewLine + "should work";
+#endif
+            // Async listen thread
+            new Thread(async () => 
+            {
+                OWO.Configure(GameAuth.Create(bs));
+
+                await OWO.AutoConnect();
+                runOnUIThread(() =>
+                {
+                    debug.Text = "CON";
+                });
+                UDPListener listener = new UDPListener(VRCPORT);
+                OscMessage messageReceived = null;
+                while (true)
+                {
+                    if (messageReceived == null)
+                        Thread.Sleep(1);
+                    else messageReceived = null;
+
+                    while (messageReceived == null)
+                    {
+                        messageReceived = (OscMessage)listener.Receive();
+                        if (messageReceived == null)
+                            Thread.Sleep(1);
+                    }
+                    
+                    if (!messageReceived.Address.StartsWith("/avatar/parameters/owo_suit_"))
+                        continue;
+
+                    if (Triggers.TryGetValue(messageReceived.Address.Substring(28), out OWOTriggerer trig)) 
+                    //{ 
+                        trig.curState = (bool)messageReceived.Arguments[0];
+                        //runOnUIThread(() =>
+                        {
+                        //    debug.Text = trig.curState.ToString();//.Substring(28);// messageReceived.Address + "  " + messageReceived.Arguments[0];
+                        }//);
+                    //}
+                }
+            }).Start();
+
+            new Thread(() =>
+            {
+                OWOTriggerer[] trigs = Triggers.Values.ToArray();
+                while(true)
+                {
+                    Thread.Sleep(300);
+                    if (OWO.ConnectionState == OWOGame.ConnectionState.Connected)
+                    {
+                        List<Muscle> targs = new List<Muscle>();
+                        foreach (OWOTriggerer trig in trigs)
+                            if (trig.curState)
+                                targs.Add(trig.Muscle);
+                        
+                        if (targs.Count > 0)
+                        OWO.Send(bs, targs.ToArray());
+                    }
+                }
+            }).Start();
+        }
+        private void runOnUIThread(Action function)
+        {
+            this.Invoke(new MethodInvoker(function));
+        }
+    }
+}
